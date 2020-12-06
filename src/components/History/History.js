06303ac87055fc7ec66d409946acbf9bf53343d4
Lucas Rodriguez <lucasrodriguezdavila@gmail.com>
2020-12-06 15:31:36 -0300

    - componente Film (contenedor de pelicula) agregado (necesita rework!)

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Image, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import { useHistory } from "react-router-dom";
 import { HiOutlineArrowCircleLeft } from "react-icons/hi";
@@ -11,24 +11,27 @@ const Background = () => {
         w="100vw"
         bg="linear-gradient(180deg, #2D0101 2.09%, #600505 78.14%, #4B0404 102.09%)"
         position="fixed"
-        zIndex="-100"></Flex>
+        zIndex="-100"
+      ></Flex>
     </>
   );
 };
 const BackButton = ({ handleGoBack }) => {
   return (
     <Flex
+      marginBottom="1rem"
       padding="2px"
       bg="linear-gradient(180deg, #B92929 0%, #C53030 15.62%, #A22424 100%)"
       boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
       borderRadius="full"
-      maxW="2.3rem"
-      maxH="2.3rem"
+      w="2.3rem"
+      h="2.3rem"
       marginTop="0.75rem"
       justifyContent="center"
       align="center"
       as="button"
-      onClick={handleGoBack}>
+      onClick={handleGoBack}
+    >
       <HiOutlineArrowCircleLeft style={{ color: "black", fontSize: "3rem" }} />
     </Flex>
   );
@@ -36,14 +39,42 @@ const BackButton = ({ handleGoBack }) => {
 const Title = ({ ...props }) => {
   return (
     <Text
+      marginLeft={["0", "1rem"]}
       fontWeight="bold"
       textShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
       color="white"
       fontSize="1.75rem"
       marginBottom="3.37rem"
-      maxW="auto">
+      maxW="auto"
+    >
       Las funciones que compraste
     </Text>
+  );
+};
+
+const Film = () => {
+  const film = {
+    title: "Capitan america",
+    price: "400",
+    date: "19/4",
+    img:
+      "https://www.cinemascomics.com/wp-content/uploads/2014/03/Capitan-America-el-Soldado-de-invierno.jpg",
+  };
+  return (
+    <Flex h="4.62rem" justifyContent="space-between">
+      <Image
+        borderRadius="36px 10px"
+        src={film.img}
+        h="4.62rem"
+        w="80%"
+        fit="cover"
+        position="absolute"
+        zIndex="-50"
+      ></Image>
+      <Flex borderRadius="36px 10px" bg="rgba(0, 0, 0, 0.61)" h="100%" w="100%">
+        HOLA
+      </Flex>
+    </Flex>
   );
 };
 
@@ -71,11 +102,19 @@ export const History = () => {
         variants={variants}
         initial="hidden"
         animate="visible"
-        exit="exit">
+        exit="exit"
+      >
         <Flex direction="column" marginTop="6.87rem" overflowX="scroll">
-          <Flex direction="column" justifyContent="center" maxW="80vw">
+          <Flex
+            direction={["column", "row"]}
+            justifyContent="center"
+            maxW="80vw"
+          >
             <BackButton />
             <Title />
+          </Flex>
+          <Flex direction="column" justifyContent="center">
+            <Film></Film>
           </Flex>
         </Flex>
       </motion.div>
