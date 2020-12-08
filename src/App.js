e8e35ef5c02396ad9e10091d2d7242606139af78
Lucas Rodriguez <lucasrodriguezdavila@gmail.com>
2020-12-08 19:12:14 -0300

    -  hook de auth agregado - servicios agregados - user context agregado

--- src/App.js
+++ src/App.js
@@ -12,40 +12,46 @@ import {
   Register,
   Profile,
 } from "./components/index";
+import { ActualUser } from "./context";
+import { useState } from "react";
 
 function App() {
+  const [actualUser, setActualUser] = useState(undefined);
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Header />
-          <Body />
-          <FooterBody />
-        </Route>
-        <Route exact path="/about">
-          <Flex direction="column" h="100vh" fontSize="1rem">
+    <ActualUser.Provider value={(actualUser, setActualUser)}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
             <Header />
-            <About />
-            <FooterAbout />
-          </Flex>
-        </Route>
-        <Route exact path="/details/:id">
-          <Header />
-          <DetailsMovies />
-          <FooterDetailsMovies />
-        </Route>
-        <Route exact path="/profile">
-          <Header />
-          <Profile />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/register">
-          <Register />
-        </Route>
-      </Switch>
-    </Router>
+            <Body />
+            <FooterBody />
+          </Route>
+          <Route exact path="/about">
+            <Flex direction="column" h="100vh" fontSize="1rem">
+              <Header />
+              <About />
+              <FooterAbout />
+            </Flex>
+          </Route>
+          <Route exact path="/details/:id">
+            <Header />
+            <DetailsMovies />
+            <FooterDetailsMovies />
+          </Route>
+          <Route exact path="/profile">
+            <Header />
+            <Profile />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
+    </ActualUser.Provider>
   );
 }
 
---
+++ src/context/index.js
@@ -0,0 +1,1 @@
+export { ActualUser } from "./actualUser.js";
